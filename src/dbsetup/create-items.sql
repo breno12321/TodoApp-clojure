31406b63c2606f55b3b3c5ea920d1ef41635dad2
breno12321 <breno.salgado05@gmail.com>
2021-03-03 23:06:56 -0300

    Fix(SQL Script): Update items table script

--- src/dbsetup/create-items.sql
+++ src/dbsetup/create-items.sql
@@ -1,5 +1,15 @@
+-- Drop table
+
+-- DROP TABLE public.items;
+
 CREATE TABLE public.items (
-  id SERIAL primary KEY,
-  title varchar(255),
-  description varchar(255)
+	id serial NOT NULL,
+	title varchar(255) NULL,
+	description varchar(255) NULL,
+	tags varchar(255) NULL,
+	done bool NOT NULL DEFAULT false,
+	created_at timestamptz(0) NOT NULL DEFAULT now(),
+	updated_at timestampz(0) NOT NULL DEFAULT now(),
+	due_date timestampz(0) NULL,
+	CONSTRAINT items_pkey PRIMARY KEY (id)
 );
